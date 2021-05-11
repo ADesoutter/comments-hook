810bb3d11b926a6171c2ac6aa3b4d2c59bea3356
Jblavisse <jean-baptiste@jblavisse.fr>
2021-05-11 17:27:41 +0200

    admin mode change with onclick

--- src/AdminMode.js
+++ src/AdminMode.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './AdminMode.css'
 
 export default function AdminMode() {
+    const [isAdmin, setIsAdmin] = useState(false)
+
+    const toggleAdminMode = () => {
+        // isAdmin ? setIsAdmin(false) : setIsAdmin(true);
+        setIsAdmin(!isAdmin);
+    }
+
     return (
         <div className="admin-mode"> 
-            <article className="message is-info">
+            <article className={isAdmin ? "message is-danger":"message is-info"}>
             <div className="message-body">
-                <button className="button is-info">Activer le mode d'administration</button>
+                <button onClick={toggleAdminMode}
+                    className={isAdmin ? "button is-danger": "button is-info"}>
+                   {isAdmin ? 
+                        "Désactiver le mode d'administration"
+                     :  "Activer le mode d'administration"} 
+                </button>
             </div>
             </article>
         </div>
