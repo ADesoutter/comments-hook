3c25e143aa2a77cba8c9607696ab91c3578f1e68
Jblavisse <jean-baptiste@jblavisse.fr>
2021-05-11 12:26:01 +0200

    Add state to comment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
 import Comments from './Comments';
-import Counter from './Counter';
 
 export default function App() {
     return (
       <div className="App container">
-        {/* <Comments /> */}
-
-        <Counter />
+        <Comments />
       </div>
     );
 }
--- src/Comment.js
+++ src/Comment.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Comment() {
+    const [pseudo, setPseudo] = useState("Jean-Paul");
+    const [message, setMessage] = useState("Lorem ipsum");
+
+
+    // La même que : 
+    // function revealSecretMessage() {}
+    const revealSecretMessage = () => {
+        setMessage("Je suis un message secret");
+    }
+
     return (
-        <li>
-            <strong>ddd</strong> <br/>
-            blablablabla
+        // La même que <li onClick={revealSecretMessage}>
+        <li onClick={() => setMessage("Je suis un message secret")}>
+            <strong>{pseudo}</strong> <br/>
+            {message}
         </li>
     )
 }
